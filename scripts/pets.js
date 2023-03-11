1820476996506d7e105707d3ad6060c9d2d5ca9d
Anthony Oleinik <antholeinik@gmail.com>
2023-03-10 20:18:43 -0600

    WIP pets display

---
+++ scripts/pets.js
@@ -0,0 +1,28 @@
+// basic http request to /pets/pets.json
+
+const request = new XMLHttpRequest();
+request.open('GET', '/pets/pets.json');
+request.responseType = 'json';
+request.send();
+
+request.onload = function () {
+    const pets = request.response;
+    const petSlot = document.getElementById('pets');
+    Object.keys(pets).forEach(petName => {
+        petSlot.innerHTML += `<pet-card><img slot="img" src="/pets/${petName}.png"><h3 slot="name">${petName}</h3><p slot="body">${pets[petName]}</p></pet-card>`;
+    });
+}
+
+customElements.define(
+    "pet-card",
+    class extends HTMLElement {
+      constructor() {
+        super();
+        let template = document.getElementById("pet-card");
+        let templateContent = template.content;
+
+        const shadowRoot = this.attachShadow({ mode: "open" });
+        shadowRoot.appendChild(templateContent.cloneNode(true));
+      }
+    }
+  );
