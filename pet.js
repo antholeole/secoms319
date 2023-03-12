b1c1e4e87c636aa9ba6ecfb2de82868370b3b9a6
ty-evans22 <114228074+ty-evans22@users.noreply.github.com>
2023-03-12 15:22:14 -0500

    added display of weight attribute

---
+++ pet.js
@@ -0,0 +1,20 @@
+const request = new XMLHttpRequest();
+request.open('GET', "data.json");
+request.responseType = 'json';
+request.send();
+
+request.onload = function () {
+    const currentPetId = new URLSearchParams(window.location.search).get('pet');
+    const currentPet = request.response.pets.find(item => item.id === currentPetId);
+
+    populatePage(currentPet.name, currentPet.desc, currentPet.weight);
+}
+
+const populatePage = (petName, petDesc, petWgt) => {
+    /// Populate the page
+    document.getElementById("pet-name").innerHTML = petName;
+    document.getElementById("pet-img").src = "/pets/" + petName + ".png";
+    document.getElementById("pet-wgt").innerHTML = petWgt + " lbs";
+    document.getElementById("pet-desc").innerHTML = petDesc;
+    console.log(petName, petDesc, petWgt)
+}
